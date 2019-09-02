6f8c5e127830aa2c4e412390727f37811f0e5f69
Liang <gaoxiaoliangz@gmail.com>
2019-09-02 15:03:32 +0800

    update views

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.resolve(__dirname, '../public')))
 
-app.set('views', path.resolve(__dirname, './views'))
+app.set('views', path.resolve(__dirname, '../views'))
 app.set('view engine', 'ejs')
 
 app.use(connectDB())
