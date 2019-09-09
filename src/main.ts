ee50f0b7f2247a041cb4e6fb4e1d67faa41cc70a
Liang <gaoxiaoliangz@gmail.com>
2019-09-09 15:59:43 +0800

    added ws

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import http from 'http'
+import ws from 'ws'
 import app from './app'
-import * as http from 'http'
 import { normalizePort } from './utils'
 
 const debug = require('debug')('myapp:main')
 
-const onError = error => {
+const port = normalizePort(process.env.PORT || '3000')
+const server = http.createServer(app)
+
+app.set('port', port)
+
+server.listen(port)
+
+server.on('error', (error: any) => {
   if (error.syscall !== 'listen') {
     throw error
   }
@@ -13,26 +21,51 @@ const onError = error => {
 
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
+      debug(bind + ' requires elevated privileges')
       process.exit(1)
       break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
+      debug(bind + ' is already in use')
       process.exit(1)
       break
     default:
       throw error
   }
-}
-
-const port = normalizePort(process.env.PORT || '3000')
-const server = http.createServer(app)
-
-app.set('port', port)
-
-server.listen(port)
-server.on('error', onError)
+})
 
 server.on('listening', () => {
   debug(`server running on http://localhost:${port}`)
+})
+
+// ws
+const wsServer = new ws.Server({
+  path: '/',
+  noServer: false,
+  port: 9999,
+})
+
+const broadcast = data => {
+  const results = []
+  for (let client of Array.from(wsServer.clients)) {
+    if (client.readyState === 1) {
+      client.send(JSON.stringify(data))
+    }
+  }
+  return results
+}
+
+const broadcastTotal = () => {
+  const total = wsServer.clients.size
+  debug(`${total} client(s) connected`)
+  broadcast({
+    type: 'update_count',
+    total,
+  })
+}
+
+wsServer.on('connection', (socket, request) => {
+  broadcastTotal()
+  socket.on('close', () => {
+    broadcastTotal()
+  })
 })
